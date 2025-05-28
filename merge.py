bd62e1e0633f1c7717cd510879ef83abedad70c5
hhzol <100747442+hhzol@users.noreply.github.com>
2025-05-28 17:41:04 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -2,11 +2,20 @@ import os
 import glob
 import xml.etree.ElementTree as ET
 
+def extract_date(filename):
+    """从文件名中提取日期"""
+    try:
+        date_str = filename.split("_")[1].split(".")[0]  # 提取 YYYYMMDD
+        return int(date_str)
+    except Exception:
+        return float('inf')  # 发生异常时，放到列表后面
+
 def merge_epg_files(output_file="all.xml"):
-    # 获取当前目录下所有epg_开头的xml文件
-    epg_files = glob.glob("epg_*.xml")
+    # 获取当前目录下所有 epg_ 开头的 xml 文件，并按日期排序
+    epg_files = sorted(glob.glob("epg_*.xml"), key=extract_date)
+    
     if not epg_files:
-        print("没有找到以epg_开头的XML文件")
+        print("没有找到以 epg_ 开头的 XML 文件")
         return
 
     merged_tree = None
@@ -33,17 +42,17 @@ def merge_epg_files(output_file="all.xml"):
         # 收集 <programme> 节点
         all_programmes.extend(root.findall("programme"))
 
-    # 清空原有<channel>和<programme>节点
+    # 清空原有 <channel> 和 <programme> 节点
     for elem in merged_root.findall("channel"):
         merged_root.remove(elem)
     for elem in merged_root.findall("programme"):
         merged_root.remove(elem)
 
-    # 添加去重后的<channel>节点
+    # 添加去重后的 <channel> 节点
     for channel in all_channels.values():
         merged_root.append(channel)
 
-    # 添加所有<programme>节点
+    # 添加所有 <programme> 节点
     for programme in all_programmes:
         merged_root.append(programme)
 
