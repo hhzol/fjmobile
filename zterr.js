557651b3c2a98b8637cfb9b457049c4e4163c39d
hhzol <100747442+hhzol@users.noreply.github.com>
2026-07-03 23:22:11 +0800

    Update zterr.js

--- zterr.js
+++ zterr.js
@@ -1,27 +1,32 @@
 function main(item) {
-    const url = item.url;
-    const id = ku9.getQuery(url, "id");
-    const playseek = ku9.getQuery(url, "playseek");
-    if (!id) {
-        return { url: url };
-    }
-    const base =
-        "http://zterr.hvs.fj.chinamobile.com:6060/PLTV/88888888/224/"
-        + id
-        + "/index.m3u8";
-    // ❗关键逻辑：判断是否回看
-    if (!playseek || playseek.length < 10) {
-        console.log("非法 playseek");
-        return { url: base + "?servicetype=1" };
-    }
-    // 回看直接用 M3U 给的 playseek
-    return {
-        url: base + "?servicetype=3&playseek=" + playseek,
-        player: 1,
-        scale: 0,
-        headers: {
-            "User-Agent": "okhttp/3.12.11"
-        }
-    };
-}
 
+    const url = item.url;
+
+    const id = ku9.getQuery(url, "id");
+    const playseek = ku9.getQuery(url, "playseek");
+
+    if (!id) {
+        return { url: url };
+    }
+
+    const base =
+        "http://zterr.hvs.fj.chinamobile.com:6060/PLTV/88888888/224/"
+        + id
+        + "/index.m3u8";
+
+    // ❗关键逻辑：判断是否回看
+    if (!playseek) {
+        // 直播（虽然你说不用直播，但必须兜底，否则有时会黑屏）
+        return { url: base + "?servicetype=1" };
+    }
+
+    // 回看直接用 M3U 给的 playseek
+    return {
+        url: base + "?servicetype=3&playseek=" + playseek,
+        player: 1,
+        scale: 0,
+        headers: {
+            "User-Agent": "okhttp/3.12.11"
+        }
+    };
+}
